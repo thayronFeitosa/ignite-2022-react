af42e7d8d8b869b54de63a5660389e767641a4b9
thayron Matos <thayron.f3@gmail.com>
2022-06-08 12:04:13 -0300

    Estrutura e estilização do formulário de comentários, aprendendo também de algumas propriedades novas como o :focus do CSS.

--- 01-fundamentos-reactjs/src/components/Post/index.jsx
+++ 01-fundamentos-reactjs/src/components/Post/index.jsx
@@ -5,16 +5,20 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/42882103?v=4"/>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/42882103?v=4"
+          />
 
           <div className={styles.authorInfo}>
             <strong>Tharyon Feitosa</strong>
             <span>Developer</span>
           </div>
-          
         </div>
 
-        <time tile="03 de junho às 03:27h" dateTime="2022-06-08 03:27:30">Publicado há 1h</time>
+        <time tile="03 de junho às 03:27h" dateTime="2022-06-08 03:27:30">
+          Publicado há 1h
+        </time>
       </header>
 
       <div className={styles.content}>
@@ -33,6 +37,15 @@ export function Post() {
           <a href="#">#rocketseat</a>{" "}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+        <textarea className={styles.s} placeholder="Deixe o seu comentário" />
+
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
     </article>
   );
 }
