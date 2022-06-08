b3b526d654c41966ae8424b414c16babd4900382
thayron Matos <thayron.matos@soluti.com.br>
2022-06-08 02:09:22 -0300

    init project classe ignite 2022

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import './global.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+    </>
+  );
+}
+
+export default App;
