855b109cc33e6788d00883f727493cac702ae8d3
thayron Matos <thayron.f3@gmail.com>
2022-06-10 02:40:26 -0300

    Utilizando estado para criar comentário

--- 01-fundamentos-reactjs/src/components/Post/index.jsx
+++ 01-fundamentos-reactjs/src/components/Post/index.jsx
@@ -3,8 +3,11 @@ import { Comment } from "../Comment";
 import styles from "./Post.module.css";
 import { format, formatDistanceToNow } from "date-fns";
 import ptBr from "date-fns/locale/pt-BR";
+import { useState } from "react";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1,2]);
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -17,6 +20,11 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBr,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -45,7 +53,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe o seu feedback</strong>
         <textarea className={styles.s} placeholder="Deixe o seu comentário" />
 
@@ -54,7 +62,9 @@ export function Post({ author, publishedAt, content }) {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
+        {comments.map((comment) =>{
+          return <Comment/>
+        })}
       </div>
     </article>
   );
