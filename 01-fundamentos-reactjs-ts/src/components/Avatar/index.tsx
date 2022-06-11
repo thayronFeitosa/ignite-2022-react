fed53a5357df0eb89b5961413ef0691abc83f9cf
thayron Matos <thayron.f3@gmail.com>
2022-06-11 01:28:03 -0300

    extendendo propriedades de uma img e tipando o nosso componente Avatar

--- 01-fundamentos-reactjs-ts/src/components/Avatar/index.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Avatar/index.tsx
@@ -1,17 +1,14 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
-
-interface AvatarPrps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt}:AvatarPrps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
